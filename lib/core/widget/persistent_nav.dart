f65f688e72c8f031387d367e7f8565bdf064f197
ArilessTir <ariless.tir@gmail.com>
2024-10-20 17:32:08 +0100

    Create profile page

--- lib/core/widget/persistent_nav.dart
+++ lib/core/widget/persistent_nav.dart
@@ -16,40 +16,136 @@ class PersistentNav extends StatefulWidget {
 class _PersistentNavState extends State<PersistentNav> {
   // Set initialIndex to 1 to make HomePage the main page
   final PersistentTabController _controller =
-      PersistentTabController(initialIndex: 1);
+      PersistentTabController(initialIndex: 0);
+  void showBottomModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 16),
+          child: SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Créer",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16),
+                const Divider(
+                  color: Color.fromARGB(34, 0, 0, 0),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(foregroundColor: Colors.black),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(PhosphorIconsFill.coatHanger),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        'Un look',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextButton(
+                  style: TextButton.styleFrom(foregroundColor: Colors.black),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  },
+                  child: const Row(
+                    children: [
+                      ImageIcon(
+                        size: 25,
+                        AssetImage(
+                          "assets/icons/nav_tab.png",
+                        ),
+                      ),
+                      SizedBox(width: 12),
+                      Text(
+                        'Un tableau',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   List<PersistentTabConfig> _tabs() => [
-        PersistentTabConfig(
-          screen: const LoginPage(),
-          item: ItemConfig(
-            activeForegroundColor: Colors.black,
-            inactiveBackgroundColor: Colors.white,
-            icon: Icon(
-              _controller.index == 0
-                  ? PhosphorIconsFill.cloudSun // Filled icon when active
-                  : PhosphorIconsBold.cloudSun, // Regular icon when inactive
-            ),
-            title: "Meteo",
-          ),
-        ),
         PersistentTabConfig(
           screen: HomePage(),
           item: ItemConfig(
             activeForegroundColor: Colors.black,
             inactiveBackgroundColor: Colors.white,
             icon: Icon(
-              _controller.index == 1
-                  ? PhosphorIconsFill.sparkle
-                  : PhosphorIconsBold.sparkle,
+              _controller.index == 0
+                  ? PhosphorIconsFill.house
+                  : PhosphorIconsBold.house,
             ),
             title: "Accueil",
+          ),
+        ),
+        PersistentTabConfig(
+          screen: const LoginPage(),
+          item: ItemConfig(
+            activeForegroundColor: Colors.black,
+            inactiveBackgroundColor: Colors.white,
+            icon: Icon(
+              _controller.index == 1
+                  ? PhosphorIconsFill.cloudSun // Filled icon when active
+                  : PhosphorIconsBold.cloudSun, // Regular icon when inactive
+            ),
+            title: "Meteo",
+          ),
+        ),
+        PersistentTabConfig.noScreen(
+          onPressed: (BuildContext context) {
+            return showBottomModal(context);
+          },
+          item: ItemConfig(
+            activeForegroundColor: Colors.black,
+            icon: Icon(
+              _controller.index == 2
+                  ? PhosphorIconsFill.plus
+                  : PhosphorIconsBold.plus,
+            ),
+            title: "Créer",
           ),
         ),
         PersistentTabConfig(
           screen: const LookPage(),
           item: ItemConfig(
             activeForegroundColor: Colors.black,
-            icon: ImageIcon(
+            icon: const ImageIcon(
               size: 25,
               AssetImage(
                 "assets/icons/nav_tab.png",
